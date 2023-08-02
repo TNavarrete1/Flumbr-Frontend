ae85e344eb47ce5f8f34bc9cce703687e2904983
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-08-02 11:41:50 -0700

    Update CI/CD region and s3 bucket name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://flumblr.us-east-1.elasticbeanstalk.com/flumblr/api',
+  apiBaseUrl:
+    'http://flumblr-env.eba-cf3cbpwf.us-west-1.elasticbeanstalk.com/flumblr/api',
   giphyApiBaseUrl: 'http://api.giphy.com/v1/gifs',
   giphyApiKey: 'jUHLiltKmmkCnx3uksK6rmXB6uc6byyB',
 };
